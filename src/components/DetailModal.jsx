fb550af328b01b7244c4775dc3169b7fddf0dbc3
he2e2 <dddd6368@naver.com>
2024-09-03 16:22:06 +0900

    feat: Added fuction to edit details

--- src/components/DetailModal.jsx
+++ src/components/DetailModal.jsx
@@ -1,6 +1,43 @@
 /* eslint-disable react/prop-types */
 
+import { useState } from "react";
+import { useSetRecoilState } from "recoil";
+
+import { updatedState } from "@atoms";
+import { validateCall, validateName } from "@shared";
+
 export function DetailModal({ item, setIsModal }) {
+  const [group, setGroup] = useState(item.group);
+  const [name, setName] = useState(item.name);
+  const [call, setCall] = useState(item.call);
+  const [detail, setDetail] = useState(item.detail);
+
+  const setIsUpdated = useSetRecoilState(updatedState);
+
+  const editInfo = () => {
+    if (validateName(name) || validateCall(call)) {
+      alert(`${validateName(name) || ""}\n${validateCall(call) || ""}`);
+      return;
+    }
+
+    const updatedItem = {
+      name,
+      call,
+      group,
+      detail,
+    };
+
+    const contactList = JSON.parse(localStorage.getItem("contactList")) || [];
+    const updatedContacts = contactList.map((contact) =>
+      contact.name === item.name ? updatedItem : contact
+    );
+
+    localStorage.setItem("contactList", JSON.stringify(updatedContacts));
+    setIsUpdated((prev) => !prev);
+    alert("수정 되었습니다!");
+    setIsModal(false);
+  };
+
   return (
     <div className="detail-wrapper">
       <button
@@ -14,21 +51,51 @@ export function DetailModal({ item, setIsModal }) {
       <section className="detail-modal">
         <h2>연락처 상세 정보</h2>
         <ul>
-          {Object.entries(item).map(([group, content]) => {
-            return <DetailItem key={group} group={group} content={content} />;
-          })}
+          <DetailItem group="name" content={name} set={setName} />
+          <DetailItem group="call" content={call} set={setCall} />
+          <DetailItem group="group" content={group} set={setGroup} />
+          <DetailItem group="detail" content={detail} set={setDetail} />
         </ul>
-        <button className="edit-button">수정</button>
+        <button className="edit-button" onClick={editInfo}>
+          수정
+        </button>
       </section>
     </div>
   );
 }
 
-function DetailItem({ group, content }) {
+const switchName = {
+  name: "이름",
+  call: "전화번호",
+  group: "그룹",
+  detail: "메모",
+};
+
+function DetailItem({ group, content, set }) {
+  const groups = JSON.parse(localStorage.getItem("group"));
   return (
     <li className="group-item">
-      <span>{group}:</span>
-      <span className="content">{content}</span>
+      <span>{switchName[group]}:</span>
+      {group !== "group" ? (
+        <input
+          className="content"
+          value={content}
+          onChange={(e) => {
+            set(e.target.value);
+          }}
+        />
+      ) : (
+        <select
+          value={content}
+          onChange={(e) => {
+            set(e.target.value);
+          }}
+        >
+          {groups.map((group) => {
+            return <option key={group}>{group}</option>;
+          })}
+        </select>
+      )}
     </li>
   );
 }
