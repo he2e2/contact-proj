0c7378e030d14af0f7d37fa4aaa9ab70bb714a96
he2e2 <dddd6368@naver.com>
2024-09-03 12:05:09 +0900

    feat: Added group features

--- src/components/SelectEl.jsx
+++ src/components/SelectEl.jsx
@@ -1,21 +1,32 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
 import { GroupModal } from "./GroupModal";
 
-export function SelectEl() {
+export function SelectEl({ groups, setSelected, setGroups }) {
   const [isModal, setIsModal] = useState(false);
+  setSelected();
   return (
     <>
       <div className="select-el">
         <label>그룹</label>
-        <select>
-          <option>가족</option>
-          <option>친구</option>
-          <option>직장</option>
-          <option>스터디</option>
+        <select
+          onChange={(e) => {
+            setSelected(e.target.value);
+          }}
+        >
+          {groups.map((group) => {
+            return <option key={group}>{group}</option>;
+          })}
         </select>
         <button onClick={() => setIsModal((prev) => !prev)}>조직추가</button>
       </div>
-      {isModal && <GroupModal />}
+      {isModal && (
+        <GroupModal
+          groups={groups}
+          setGroups={setGroups}
+          setIsModal={setIsModal}
+        />
+      )}
     </>
   );
 }
